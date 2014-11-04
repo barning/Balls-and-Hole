58c0f367667fb7a421a30b510a86be6815ec9c81
Niklas Barning <niklas.barning@hfk-bremen.de>
2014-11-04 22:19:29 +0100

    Added Jump

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,6 +5,7 @@ public class Controller : MonoBehaviour {
 
 	public float playerRotationSpeed = 5.0f;
 	public float playerSpeed = 5.0f;
+	public float JumpSpeed = 5.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,5 +15,13 @@ public class Controller : MonoBehaviour {
 	void Update () {
 		transform.Translate(Vector3.right * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
 		transform.Rotate (Vector3.up * Input.GetAxis ("Horizontal") * playerRotationSpeed * Time.deltaTime);
+		if (Input.GetButtonDown("Jump")) {
+			print("yeah");
+			jump();
+		}
 	}
+
+	void jump(){
+		rigidbody.AddForce (Vector3.up * JumpSpeed);
+		}
 }
